28d80c879aa259ac8dfcabf7be7d17ea97bd6811
Colin Copeland <copelco@caktusgroup.com>
2022-07-20 23:16:41 -0400

    add search_type facet

--- example-facets/traffic_stops/filters.py
+++ example-facets/traffic_stops/filters.py
@@ -1,15 +1,12 @@
-import django_filters
-
 from .models import Stop
 from django_filters_facet import Facet, FacetedFilterSet
 
 
 class StopFilterSet(FacetedFilterSet):
-    stop_purpose = django_filters.ChoiceFilter(choices=Stop.PURPOSE_CHOICES)
-
     class Meta:
         model = Stop
-        fields = ["stop_purpose"]
+        fields = ["stop_purpose", "search_type"]
 
     def configure_facets(self):
         self.filters["stop_purpose"].facet = Facet()
+        self.filters["search_type"].facet = Facet()
