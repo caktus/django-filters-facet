5d2390b36079645fbc95befb06d523c35ec89bed
Jason Judkins <jjudkins@caktusgroup.com>
2022-08-26 16:42:52 -0400

    film search setup

--- example-facets/films/filters.py
+++ example-facets/films/filters.py
@@ -1,9 +1,15 @@
+import django_filters
+
+from django.contrib.postgres import search
+
 from django_filters_facet import Facet, FacetedFilterSet
 
 from .models import Film
 
 
 class FilmFilterSet(FacetedFilterSet):
+    search = django_filters.CharFilter(method="filter_search", label="Search")
+
     class Meta:
         model = Film
         fields = ["type", "release_year", "rating", "listed_in"]
@@ -13,3 +19,21 @@ class FilmFilterSet(FacetedFilterSet):
         self.filters["release_year"].facet = Facet()
         self.filters["rating"].facet = Facet()
         self.filters["listed_in"].facet = Facet()
+
+    def filter_search(self, queryset, name, value):
+        vector = search.SearchVector("title", weight="B")
+        query = search.SearchQuery(value)
+        return (
+            queryset.annotate(
+                rank=search.SearchRank(vector, query),
+                headline=search.SearchHeadline(
+                    "title",
+                    query,
+                    min_words=30,
+                    start_sel="<mark>",
+                    stop_sel="</mark>",
+                ),
+            )
+            .filter(rank__gt=0.1)
+            .order_by("-rank")
+        )
